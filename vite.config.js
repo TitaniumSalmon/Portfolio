a405634c3a22f0ae61034cf7c26021e31e9cfe59
TitaniumSalmon <perapon.s@ku.th>
2026-09-07 23:17:06 +0700

    fix default path

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/vite_700/',
+  base: '/Portfolio/',
   plugins: [react()],
   port: 5500,
 })
